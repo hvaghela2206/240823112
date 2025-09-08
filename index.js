85f9c3c7692d2dc69a2bb868b498d88449921736
hvaghela <heenavaghela978@gmail.com>
2025-09-08 09:07:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-import add from "./add.js"
-console.log (add(11,12));
-console.log(add(33,22));
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://heenavaghela978_db_user:H7753034@heena.yz1n1wq.mongodb.net/students",{
+    // userNewUrlParser:true,
+    // useUnifiedTopology:true,
+})
+
+const studentSchema = new mongoose.Schema({
+    name:String,
+    department:String,
+
+})
+
+const Student = mongoose.model("student",studentSchema)
+
+console.log("mongodb connected successfuly")
+
+const student = new Student({
+    name:"jeel",
+    department:"computer"
+})
+
+student.save().then(()=>{console.log("student saved succesfully")}).catch((error)=>{console.log("error",error)})
